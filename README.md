8169de0733bd0d04dd69e54691172908d7db2f47
nzina <nigusdev@gmail.com>
2023-11-19 22:04:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 3mvkxb-fftafj
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/nzina/3mvkxb-fftafj)
---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,12 @@
+import { CartService } from '../cart.service';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css'],
+})
+export class CartComponent {
+  items = this.cartService.getItems();
+  constructor(private cartService: CartService) {}
+}
